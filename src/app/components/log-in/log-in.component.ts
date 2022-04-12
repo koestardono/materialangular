0812dc874f457911d43c66730223cea9446a14cd
Koestardono <koestardono@infinetworks.com>
2022-04-12 16:05:42 +0700

    auth not finish

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  isSubmitting = false;
+  loginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    rememberMe: [false],
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    // private router: Router,
+    // private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
