8ff44135aca176b638f699dd69f5d41c40f1931b
Koestardono <koestardono@infinetworks.com>
2022-05-11 11:20:09 +0700

    test push new branch

--- src/app/components/mydata/myprofile/myprofile.component.ts
+++ src/app/components/mydata/myprofile/myprofile.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyprofileComponent implements OnInit {
 
+  // Test push new branch !
   constructor() { }
 
   ngOnInit(): void {
